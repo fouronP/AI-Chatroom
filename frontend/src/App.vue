d09cbddee43da2c12b1108e6f5ca2a13e9a7323a
fouronP <3609763561@qq.com>
2026-07-13 14:44:42 +0800

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// 应用根组件
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background-color: #f0f2f5;
+}
+
+#app {
+  height: 100vh;
+}
+</style>
